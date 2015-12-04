77829a405622490c1be6093a10c139dfdce6b143
MartinSandstrom <Martin.sandstrom@avanade.com>
2015-12-04 16:19:44 +0100

    Solving second part of day one.

---
+++ dayOnePartTwo.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+fs.readFile("test-data/day-one.txt", "utf8", function (error, data) {
+    if(error) {
+        console.log(error);
+    }
+
+    console.log('answer', findIndex(data));
+
+});
+
+function findIndex(inputString){
+    var total = 0;
+    for (var i = 0; i < inputString.length; i++) {
+        if(inputString[i] === ')') {
+            total--;
+        }
+        else if( inputString[i] === '('){
+            total++;
+        }
+        if(total < 0) return i + 1;
+    }
+}
+
+
+
+console.log('1', findIndex(')'));
+console.log('5', findIndex('()())'));
