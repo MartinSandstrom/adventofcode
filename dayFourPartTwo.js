3cbdf37126428dfad954fc3c52748b2be83f41f0
MartinSandstrom <Martin.sandstrom@avanade.com>
2015-12-04 18:39:34 +0100

    Sovling day four part two.

---
+++ dayFourPartTwo.js
@@ -0,0 +1,16 @@
+var crypto = require('crypto');
+
+var md5sum = crypto.createHash('md5');
+
+console.log('answer', test('yzbqklnj'));
+
+function test(input){
+    for (var i = 100000; i < 100000000; i++) {
+        var test = crypto.createHash('md5').update(input + i).digest("hex");
+        if(isCorrect(test)) return i;
+    }
+}
+
+function isCorrect(input){
+    return input.slice(0, 6) == '000000';
+}
