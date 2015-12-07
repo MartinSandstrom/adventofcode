8a8517d02fbada77304ed7b7172490bcf4c58f9e
MartinSandstrom <Martin.sandstrom@avanade.com>
2015-12-07 10:23:36 +0100

    Saving kind of working solution, something is still wrong.

--- solutions/7/a.js
+++ solutions/7/a.js
@@ -1,42 +1,48 @@
 var fs = require('fs');
 var answerObj = {};
+var inputStrings = [];
 
 fs.readFile('../../test-data/day-seven.txt', 'utf8', function (error, data) {
 	if (error) {
 		console.log(error);
 	}
-	var inputStrings = data.split(/\n/);
-	var start = '';
-	for (var i = 0; i < inputStrings.length; i++) {
-		if (inputStrings[i].indexOf('44430') > -1) {
-			start = inputStrings[i];
+	inputStrings = data.split(/\r\n/);
+	inputString = inputStrings.filter(Boolean);
+	while (!answerObj['a']) {
+		var command = findNextCommand();
+		if(command) {
+			console.log('running command: ', command);
+			populateAnswerObj(command);
 		}
 	}
-	populateAnswerObj(start.toString());
-	while (inputStrings) {
-		var command = findNextCommand(inputStrings);
-		console.log(command);
-		populateAnswerObj(command);
-		inputStrings.splice(inputStrings.indexOf(command), 1);
-	}
-	console.log(answerObj);
+	console.log(answerObj.a);
 });
 
-function findNextCommand(inputStrings) {
+
+
+function findNextCommand() {
 	for (var i = 0; i < inputStrings.length; i++) {
 		var information = inputStrings[i].split(' ');
-		information.splice(information.indexOf('NOT'), 1);
-		information.splice(information.indexOf('AND'), 1);
-		information.splice(information.indexOf('->'), 1);
-		information.splice(information.indexOf('LSHIFT'), 1);
-		information.splice(information.indexOf('RSHIFT'), 1);
-		console.log(information);
-		if (information.length > 2) {
-			if (answerObj[information[0]] && answerObj[information[1]]) {
-				return inputStrings[i];
+		if((information.indexOf('AND') > -1) || (information.indexOf('OR') > -1)) {
+			if ((answerObj[information[0]] || !isNaN(information[0])) && (answerObj[information[2]]|| !isNaN(information[2]))) {
+				var command = inputStrings.splice(i, 1);
+				return command[0];
 			}
-		} else if (answerObj[information[0]]) {
-			return inputStrings[i];
+		} else if(information.indexOf('LSHIFT') > -1 || information.indexOf('RSHIFT') > -1) {
+			if (answerObj[information[0]] || !isNaN(information[0])) {
+					var command = inputStrings.splice(i, 1);
+				return command[0];
+			}
+		}else if(inputStrings[i].indexOf('NOT') > -1) {
+			if (answerObj[information[1]] || !isNaN(information[1])) {
+				var command = inputStrings.splice(i, 1);
+				return command[0];
+			}
+		} else if(inputStrings[i].indexOf('->') > -1) {
+			if (answerObj[information[0]] || !isNaN(information[0])) {
+				var command = inputStrings.splice(i, 1);
+				return command[0];
+			}
 		}
 	}
 }
@@ -47,10 +53,14 @@ function populateAnswerObj(inputCommand) {
 	var f = information[0];
 	var s = information[2];
 	if (isNaN(number) && inputCommand.indexOf('AND') > -1) {
-		var andValue = answerObj[f].toString(2) & answerObj[s].toString(2);
+		var first = isNaN(f) ? answerObj[f].toString(2) : f.toString(2);
+		var second = isNaN(s) ? answerObj[s].toString(2) : s.toString(2);
+		var andValue = first & second;
 		answerObj[information[information.length - 1]] = andValue.toString(10);
 	} else if (inputCommand.indexOf('OR') > -1) {
-		var orValue = answerObj[f].toString(2) | answerObj[s].toString(2);
+		var first = isNaN(f) ? answerObj[f].toString(2) : f.toString(2);
+		var second = isNaN(s) ? answerObj[s].toString(2) : s.toString(2);
+		var orValue = first | second;
 		answerObj[information[information.length - 1]] = orValue.toString(10);
 	} else if (inputCommand.indexOf('LSHIFT') > -1) {
 		var lsValue = information[2];
@@ -65,17 +75,20 @@ function populateAnswerObj(inputCommand) {
 		var a = answerObj[n].toString(2);
 		var value = ~(a);
 		answerObj[information[information.length - 1]] = (65536 + parseInt(value.toString(10)));
+	} else if(inputCommand.indexOf('->') > -1 && isNaN(number)) {
+		answerObj[information[information.length - 1]] =  answerObj[information[0]];
 	} else {
 		answerObj[information[information.length - 1]] = number;
 	}
 }
-
-// populateAnswerObj('123 -> x');
-// populateAnswerObj('456 -> y');
-// populateAnswerObj('x AND y -> d');
-// populateAnswerObj('x OR y -> e');
-// populateAnswerObj('x LSHIFT 2 -> f');
-// populateAnswerObj('y RSHIFT 2 -> g');
-// populateAnswerObj('NOT x -> h');
-// populateAnswerObj('NOT y -> i');
-// console.log(answerObj);
+/*
+populateAnswerObj('123 -> x');
+populateAnswerObj('456 -> y');
+populateAnswerObj('x AND y -> d');
+populateAnswerObj('x OR y -> e');
+populateAnswerObj('x LSHIFT 2 -> f');
+populateAnswerObj('y RSHIFT 2 -> g');
+populateAnswerObj('NOT x -> h');
+populateAnswerObj('NOT y -> i');
+console.log(answerObj);
+*/
