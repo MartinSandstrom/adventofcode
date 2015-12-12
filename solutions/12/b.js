fc3a01e8872c655423f29e4043cf30545d8e1ed1
Martin Sandström <maartiin_91@hotmail.com>
2015-12-12 15:38:41 +0100

    Cleaning.

--- solutions/12/b.js
+++ solutions/12/b.js
@@ -7,7 +7,6 @@ fs.readFile('../../test-data/day-twelve.txt', 'utf8', function (error, data) {
 
 function sumObj(data) {
 	if (typeof data === 'object' && !Array.isArray(data)) {
-		console.log('hej');
 		for (var k in data) {
 			if (data[k] === 'red') {
 				return 0;
@@ -24,29 +23,3 @@ function sumObj(data) {
 	}
 	return total;
 }
-
-/*
-	console.log(objArr);
-	if (objArr instanceof Object) {
-		for (var el in objArr) {
-			if (objArr.hasOwnProperty(el)) {
-				var isRed = false;
-				for (var le in objArr) {
-					if (objArr.hasOwnProperty(el)) {
-						if (le.indexOf('red') > -1) {
-							isRed = true;
-						}
-					}
-				}
-				if (!isRed) {
-					total += parseInt(objArr[el], 10);
-				}
-			}
-		}
-	} else {
-		for (var i = 0; i < objArr.length; i++) {
-			total += parseInt(objArr[i], 10);
-		}
-	}
-
-	*/
