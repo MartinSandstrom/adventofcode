9e50c4ead4a857225a2ea91db9dc32ad6a1286fb
Martin Sandström <maartiin_91@hotmail.com>
2015-12-18 17:06:03 +0100

    Solving day 18 part two.

---
+++ solutions/18/b.js
@@ -0,0 +1,65 @@
+
+var fs = require('fs');
+var testString = '.#.#.#...##.#....#..#...#.#..#####..';
+var current = testString;
+var next = '';
+
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index + character.length);
+}
+
+fs.readFile('../../test-data/day-18.txt', 'utf8', function (error, data) {
+	var d = data.split('\n');
+	var s = d.join('');
+	s = s.replaceAt(0, '#');
+	s = s.replaceAt(99, '#');
+	s = s.replaceAt(9999, '#');
+	s = s.replaceAt(9900, '#');
+	current = s;
+	for (var i = 0; i < 100; i++) {
+		next = '';
+		getNextState(current);
+		current = next;
+	}
+	var count = (current.match(/#/g) || []).length;
+	console.log(count);
+});
+
+function getNextState(state) {
+	for (var i = 0; i < state.length; i++) {
+		if (i === 0 || i === 99 || i === 9999 || i === 9900) {
+			next += '#';
+		} else {
+			var numberOfLit = getNumberOfNeighbors(i);
+			if (state[i] === '#') {
+				if (numberOfLit === 2 || numberOfLit === 3) {
+					next += '#';
+				} else {
+					next += '.';
+				}
+			} else if (numberOfLit === 3) {
+				next += '#';
+			} else {
+				next += '.';
+			}
+		}
+	}
+}
+
+function getNumberOfNeighbors(i) {
+	var numberOfLit = 0;
+	var neighbors = [];
+	if ((i + 1) % 100 === 0) {
+		neighbors = [current[i - 101], current[i - 100], current[i - 1], current[i + 99], current[i + 100]];
+	} else if ((i === '0') || (i % 100 === 0)) {
+		neighbors = [current[i - 100], current[i - 99], current[i + 1], current[i + 100], current[i + 101]];
+	} else {
+		neighbors = [current[i - 101], current[i - 100], current[i - 99], current[i - 1], current[i + 1], current[i + 99], current[i + 100], current[i + 101]];
+	}
+	for (var j = 0; j < neighbors.length; j++) {
+		if (neighbors[j] === '#') {
+			numberOfLit++;
+		}
+	}
+	return numberOfLit;
+}
