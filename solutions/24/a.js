1cc547ff83e182355323df178bd9abc14f937c02
Martin Sandström <maartiin_91@hotmail.com>
2015-12-24 10:58:51 +0100

    CHORE.

--- solutions/24/a.js
+++ solutions/24/a.js
@@ -22,22 +22,22 @@ for (var l = 0; l < a.length; l++) {
 console.log(min);
 
 function combine(a, min, max) {
-    var fn = function(n, src, got, all) {
-        if (n == 0) {
-            if (got.length > 0) {
-                all[all.length] = got;
-            }
-            return;
-        }
-        for (var j = 0; j < src.length; j++) {
-            fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
-        }
-        return;
-    }
-    var all = [];
-    for (var i = min; i < max; i++) {
-        fn(i, a, [], all);
-    }
-    all.push(a);
-    return all;
+	var fn = function (n, src, got, all) {
+		if (n === 0) {
+			if (got.length > 0) {
+				all[all.length] = got;
+			}
+			return;
+		}
+		for (var j = 0; j < src.length; j++) {
+			fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+		}
+		return;
+	};
+	var all = [];
+	for (var i = min; i < max; i++) {
+		fn(i, a, [], all);
+	}
+	all.push(a);
+	return all;
 }
