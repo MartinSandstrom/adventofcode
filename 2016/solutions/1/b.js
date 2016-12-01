ff4bf446e5e345b7f97aaa322cd471f8d8732393
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-01 10:55:33 +0100

    Small refactoring

--- 2016/solutions/1/b.js
+++ 2016/solutions/1/b.js
@@ -6,7 +6,6 @@ fs.readFile("../../test-data/day-one.txt", "utf8", function (error, data) {
     }
     
     doMagic(data);
-    console.log(locations);
     console.log(currentLocation);
     console.log('Answer:', x + y);
     
@@ -22,19 +21,15 @@ var currentLocation = '0,0';
 var doMagic = (data) => {
     var array = data.split(', ');
     array.find((input) => {
-        var newIndex;
-        var isRight = input[0] === 'R';
-        if(isRight) {
-            index++;
-            if(index > 3) {
-                index = 0;
-            }
-        } else {
-            index--; 
-            if ( index < 0) {
-                index = 3;
-            }
+        
+        handleIndex(input[0]);
+        
+        if(index > 3) {
+            index = 0;            
+        } else if (index < 0) {
+            index = 3;
         }
+            
         var numberToUse = input.replace('R', '').replace('L', '');
         if(!calculateNewPos(numberToUse)){
             return true;    
@@ -42,45 +37,51 @@ var doMagic = (data) => {
     });
 };
 
+var handleIndex = (input) => {
+    return input === 'R' ? index++ : index--;
+};
+
 var calculateNewPos = (numberToUse) => {
     var position = positions[index];
     var newLocation;
     var i;
-    if(position === 'N') {
-        for (i = 0; i < numberToUse; i++) {
-            y++;
-            newLocation = x + ',' + y;
-            if(hasBeenVisited(newLocation)) {
-                return false;
+    switch (position) {
+        case 'N':
+            for (i = 0; i < numberToUse; i++) {
+                y++;
+                newLocation = x + ',' + y;
+                if(hasBeenVisited(newLocation)) {
+                    return false;
+                }
             }
-        }
-    }
-    if(position === 'E'){
-        for (i = 0; i < numberToUse; i++) {
-            x++;
-            newLocation = x + ',' + y;
-            if(hasBeenVisited(newLocation)) {
-                return false;
+            break;
+        case 'E':
+            for (i = 0; i < numberToUse; i++) {
+                x++;
+                newLocation = x + ',' + y;
+                if(hasBeenVisited(newLocation)) {
+                    return false;
+                }
             }
-        }
-    } 
-    if(position === 'S'){
-        for (i = 0; i < numberToUse; i++) {
-            y--;
-            newLocation = x + ',' + y;
-            if(hasBeenVisited(newLocation)) {
-                return false;
+            break;
+        case 'S':
+            for (i = 0; i < numberToUse; i++) {
+                y--;
+                newLocation = x + ',' + y;
+                if(hasBeenVisited(newLocation)) {
+                    return false;
+                }
             }
-        }
-    } 
-    if(position === 'W') {
-        for (i = 0; i < numberToUse; i++) {
-            x--;
-            newLocation = x + ',' + y;
-            if(hasBeenVisited(newLocation)) {
-                return false;
+            break;
+        default:
+            for (i = 0; i < numberToUse; i++) {
+                x--;
+                newLocation = x + ',' + y;
+                if(hasBeenVisited(newLocation)) {
+                    return false;
+                }
             }
-        }
+            break;
     }
     return true;
 };
