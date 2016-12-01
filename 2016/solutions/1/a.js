62c4ceed601c5657a3bc0a6fd56b90d1cf11843a
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-01 09:41:44 +0100

    Solving day one

--- 2016/solutions/1/a.js
+++ 2016/solutions/1/a.js
@@ -4,13 +4,44 @@ fs.readFile("../../test-data/day-one.txt", "utf8", function (error, data) {
     if(error) {
         console.log(error);
     }
-    console.log('answer', doMagic(data));
+    doMagic(data);
+    console.log('Answer:', x + y);
 });
 
+var positions = ['N', 'E', 'S', 'W'];
+var index = 0;
+var x = 0;
+var y = 0;
 
-function doMagic(data){
-    console.log(data);
+var doMagic = (data) => {
+    var array = data.split(', ');
+    array.forEach((input) => {
+        var newIndex;
+        var isRight = input[0] === 'R';
+        if(isRight) {
+            index++;
+            if(index > 3) {
+                index = 0;
+            }
+        } else {
+            index--; 
+            if ( index < 0) {
+                index = 3;
+            }
+        }
+        var numberToUse = input.replace('R', '').replace('L', '');
+        calculateNewPos(numberToUse);
+    });
+};
+
+var calculateNewPos = (numberToUse) => {
+    var position = positions[index];
+    if(position === 'N') y += parseInt(numberToUse);
+    if(position === 'E') x += parseInt(numberToUse);
+    if(position === 'S') y -= parseInt(numberToUse);
+    if(position === 'W') x -= parseInt(numberToUse);
 }
+
 
 //console.log(')))', count(')))'));
 //console.log('(((', count('((('));
