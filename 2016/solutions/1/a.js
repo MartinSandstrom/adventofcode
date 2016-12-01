b540743633cf3475c3a8abbd499d8ea8fccf9303
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-01 13:16:05 +0100

    Small refactoring

--- 2016/solutions/1/a.js
+++ 2016/solutions/1/a.js
@@ -16,13 +16,11 @@ var y = 0;
 var doMagic = (data) => {
     var array = data.split(', ');
     array.forEach((input) => {
-        handleIndex(input[0]);
         
-        if(index > 3) {
-            index = 0;            
-        } else if (index < 0) {
-            index = 3;
-        }
+        handleIndex(input[0]);
+        if(index > 3) index = 0;            
+        else if (index < 0) index = 3;
+        
         var numberToUse = input.replace('R', '').replace('L', '');
         calculateNewPos(numberToUse);
     });
@@ -31,9 +29,8 @@ var doMagic = (data) => {
 var handleIndex = (input) => input === 'R' ? index++ : index--;
 
 var calculateNewPos = (numberToUse) => {
-    var position = positions[index];
-    if(position === 'N') y += parseInt(numberToUse);
-    if(position === 'E') x += parseInt(numberToUse);
-    if(position === 'S') y -= parseInt(numberToUse);
-    if(position === 'W') x -= parseInt(numberToUse);
+    if(positions[index] === 'N') y += parseInt(numberToUse);
+    if(positions[index] === 'E') x += parseInt(numberToUse);
+    if(positions[index] === 'S') y -= parseInt(numberToUse);
+    if(positions[index] === 'W') x -= parseInt(numberToUse);
 };
