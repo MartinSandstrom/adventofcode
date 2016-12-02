334cdd633d86a5003c6d23d4dc43eede86c7fc49
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-02 09:03:14 +0100

    Solving day two part one

--- 2016/solutions/2/a.js
+++ 2016/solutions/2/a.js
@@ -5,9 +5,45 @@ fs.readFile("../../test-data/day-two.txt", "utf8", function (error, data) {
         console.log(error);
     }
     doMagic(data);
-    console.log('Answer:');
+    var cord = x + ',' + y;
+    console.log('Answer:', code);
 });
+var code = '';
+var x = 0;
+var y = 0;
+var options = {
+    '1,-1': '1',    
+    '1,0': '2',
+    '1,1': '3',
+    '0,-1': '4',
+    '0,0': '5',
+    '0,1': '6',
+    '-1,-1': '7',
+    '-1,0': '8',
+    '-1,1': '9'
+};
 
 var doMagic = (data) => {
-    console.log('woho');
+    var tempX = 0;
+    var texmpY = 0;
+    data.split(/\r|\n/).forEach((row, index) => {
+        row.split('').forEach((input) => {
+            var tempX = x;
+            var tempY = y;
+            if(input === 'U') tempY++;
+            if(input === 'R') tempX++;
+            if(input === 'D') tempY--;
+            if(input === 'L') tempX--;
+            
+            var newPostition = tempY + ',' + tempX;
+            if(options[newPostition] > -1) {
+                x = tempX;
+                y = tempY;
+            }
+        });
+        var cord = y + ',' + x;
+        code += options[cord];
+    });
 };
+
+
