09e5ff880693494cdd07c5085f949bae2d054718
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-03 10:04:02 +0100

    Refactoring a bit

--- 2016/solutions/3/a.js
+++ 2016/solutions/3/a.js
@@ -10,7 +10,7 @@ fs.readFile("../../test-data/day-three.txt", "utf8", function (error, data) {
 
 var numberOfTriangles = 0;
 
-var doMagic = (data) => data.split(/\r|\n/).forEach((row) => isTriangle(row.split(' ').filter(Boolean).sort((a,b) => a - b)) ? numberOfTriangles++ : true);
+var doMagic = (data) => data.split(/\r|\n/).forEach((row) => isTriangle(row.split(' ').filter(Boolean).sort((a, b) => a - b)) ? numberOfTriangles++ : true);
 
 var isTriangle = (numbers) => parseInt(numbers[0]) + parseInt(numbers[1]) > parseInt(numbers[2]);
 
--- 2016/solutions/3/b.js
+++ 2016/solutions/3/b.js
@@ -10,7 +10,6 @@ fs.readFile("../../test-data/day-three.txt", "utf8", function (error, data) {
 
 var numberOfTriangles = 0;
 
-
 var doMagic = (data) => {
     var inputs = data.split(/\r|\n/);
     inputs.forEach((part, index, theArray) => theArray[index] = part.split(' ').filter(Boolean));
@@ -20,23 +19,21 @@ var doMagic = (data) => {
         var b = inputs[i][1];
         var c = inputs[i][2];
         
-        var d = inputs[i+1][0];
-        var e = inputs[i+1][1];
-        var f = inputs[i+1][2];
+        var d = inputs[i + 1][0];
+        var e = inputs[i + 1][1];
+        var f = inputs[i + 1][2];
         
-        var g = inputs[i+2][0];
-        var h = inputs[i+2][1];
-        var j = inputs[i+2][2];
+        var g = inputs[i + 2][0];
+        var h = inputs[i + 2][1];
+        var j = inputs[i + 2][2];
         
-        var first = [a,d,g].sort((one, two) => one - two);
-        var second = [b,e,h].sort((one, two) => one - two);
-        var third = [c,f,j].sort((one, two) => one - two);
-        
-        if(isTriangle(first)) numberOfTriangles++;
-        if(isTriangle(second)) numberOfTriangles++;
-        if(isTriangle(third)) numberOfTriangles++;
+        if(isTriangle([a, d, g].sort(sortNumber))) numberOfTriangles++;
+        if(isTriangle([b, e, h].sort(sortNumber))) numberOfTriangles++;
+        if(isTriangle([c, f, j].sort(sortNumber))) numberOfTriangles++;
     }
 };
+
+var sortNumber = (a, b) => a - b;
 
 var isTriangle = (numbers) => parseInt(numbers[0]) + parseInt(numbers[1]) > parseInt(numbers[2]);
 
