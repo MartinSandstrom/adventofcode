cd72d839b6954aad3ce841a2aa39c2fa1123db63
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-05 08:52:02 +0100

    Solving day five part one

--- 2016/solutions/5/a.js
+++ 2016/solutions/5/a.js
@@ -1,13 +1,26 @@
 var fs = require('fs');
+var crypto = require('crypto');
 
 fs.readFile("../../test-data/day-five.txt", "utf8", function (error, data) {
     if(error) {
         console.log(error);
     }
     doMagic(data);
-    console.log(numberOfTriangles);
 });
 
+var input = 'cxdnnyjw';
+var results = '';
+
 var doMagic = (data) => {
-    console.log('hej');
-}
+    var numberOfFound = 0;
+    var i = 0;
+    while(numberOfFound < 8) {
+        var next = crypto.createHash('md5').update(input + i).digest('hex');
+        i++;
+        if(next.slice(0, 5) === '00000') {
+            results += next[5];
+            numberOfFound++;
+        }
+    }
+    console.log(results);
+};
