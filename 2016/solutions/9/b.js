d9b4d8939b64e33e0440d29ecbc804072b6cf84e
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-09 16:42:17 +0100

    Renaming

--- 2016/solutions/9/b.js
+++ 2016/solutions/9/b.js
@@ -12,12 +12,12 @@ var doMagic = (data) => {
     for (var i = 0; i < data.length; i++) {
         if (data[i] !== '(') continue;
         var match = data.substr(i).match(/^\((\d+)x(\d+)\)/);
-        var matchLength = parseInt(match[1], 10);
-        var times = parseInt(match[2], 10);
+        var numberOfChars = parseInt(match[1], 10);
+        var instanses = parseInt(match[2], 10);
         var start = i + match[0].length;
-        var matchStr = data.substr(start, matchLength);
+        var matchStr = data.substr(start, numberOfChars);
         var decompressedLength = doMagic(matchStr);
-        length += decompressedLength * times - matchStr.length - match[0].length;
+        length += decompressedLength * instanses - matchStr.length - match[0].length;
         i = start + matchStr.length - 1;
     }
     return length;
