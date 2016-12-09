ba2f74565a39d992bb246a06f6fb9bd5501bfb4b
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-09 11:25:12 +0100

    Solving day nine part one

--- 2016/solutions/9/a.js
+++ 2016/solutions/9/a.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
-fs.readFile("../../test-data/day-nine-ex.txt", "utf8", function (error, data) {
+fs.readFile("../../test-data/day-nine.txt", "utf8", function (error, data) {
     if(error) {
         console.log(error);
     }
     
     doMagic(data);
+    var testString = 'X(3x3)ABC(3x3)ABCY'; 
+    console.log('Correct: ', testString);
+    console.log('IsCorret:', newString === testString);
+    console.log(newString.length);
+    
 });
 
 var newString = '';
@@ -20,10 +25,14 @@ var doMagic = (data) => {
             var info = first + data.substr(0, data.indexOf(')') + 1);
             data = data.substring(data.indexOf(')') + 1);
             var replaced = info.replace('(', '').replace(')', '');
-            var array = info.split('x');
+            var array = replaced.split('x');
             var numberOfChars = array[0];
             var instanses = array[1];
-            var dataToUse = '';
+            var stringToReplicate = data.substr(0, numberOfChars);
+            for (var i = 0; i < instanses; i++) {
+                newString += stringToReplicate;
+            }
+            data = data.substring(numberOfChars);
         }
     }
     console.log(newString);
