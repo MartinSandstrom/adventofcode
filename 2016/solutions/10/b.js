981bacaaaf70c527ce38ea30f491ad38e88bce3e
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2016-12-10 14:02:14 +0100

    Refactor

--- 2016/solutions/10/b.js
+++ 2016/solutions/10/b.js
@@ -4,9 +4,7 @@ fs.readFile("../../test-data/day-ten.txt", "utf8", function (error, data) {
     if(error) {
         console.log(error);
     }
-    
     doMagic(data);
-    
 });
 var bots = {};
 var outputs = {};
@@ -17,71 +15,44 @@ var doMagic = (data) => {
     
     var lines = data.split(/\r|\n/);
     var hasFoundOne = true;
+    var bot;
 
     while (hasFoundOne) {
         hasFoundOne = false;
         lines.forEach((row, index, obj) => {
             var info = row.split(' ');
             if(info[0] === 'value') {
-                var mbot = info[5];
+                bot = info[5];
                 var value = info[1];
-                giveBotValue(mbot, value);
+                if(!bots[bot]) bots[bot] = { low: 0, high: 0};
+                updateBot(bot, value);
                 obj.splice(index, 1);
                 hasFoundOne = true;
             } else if(info[0] === 'bot') {
                 var numberLow = info[6];
                 var numberHigh = info[11];
-                var bot = info[1];
-                if(!bots[bot]) {
-                    bots[bot] = { low: 0, high: 0};
-                }
+                bot = info[1];
+                if(!bots[bot]) bots[bot] = { low: 0, high: 0};
+                
                 if(bots[bot].high !== 0 && bots[bot].low !== 0) {
-                    if(info[5] === 'output') {
-                        giveOutputValue(numberLow, parseInt(bots[bot].low));
-                        bots[bot].low = 0;
-                    } else {
-                        giveBotValue(numberLow, parseInt(bots[bot].low));
-                        bots[bot].low = 0;
-                    } 
-                    if(info[10] === 'output') {
-                        giveOutputValue(numberHigh, parseInt(bots[bot].high));
-                        bots[bot].high = 0;
-                    } else {
-                        giveBotValue(numberHigh, parseInt(bots[bot].high));
-                        bots[bot].high = 0;
-                    } 
+                    if(info[5] === 'output') updateOutput(numberLow, bots[bot].low);
+                    else updateBot(numberLow, bots[bot].low);
+                    
+                    if(info[10] === 'output') updateOutput(numberHigh, bots[bot].high);
+                    else updateBot(numberHigh, bots[bot].high);
+                    
+                    bots[bot].low = 0;
+                    bots[bot].high = 0;
                     hasFoundOne = true;
                 }
             }
         });
     }
-    
-    console.log(outputs[0].high * outputs[1].high * outputs[2].high);
-    
-    //console.log(outputs);
-    //console.log('--------');
-    //console.log(output);
-    //console.log('--------');
-    //console.log(bots);
-    //console.log(lines);
-    
-};
-
-var giveBotValue = (bot, value) => {
-    if(!bots[bot]) {
-        bots[bot] = { low: 0, high: 0};
-    }
-    updateBot(bot, value);
-};
-
-giveOutputValue = (output, value) => {
-    if(!outputs[output]) {
-        outputs[output] = { low: 0, high: 0};
-    }
-    updateOutput(output, value);
+    console.log(outputs[0] * outputs[1] * outputs[2]);
 };
 
 var updateBot = (bot, value) => {
+    value = parseInt(value);
     if(value > bots[bot].high) {
         bots[bot].low = bots[bot].high;
         bots[bot].high = value;
@@ -90,12 +61,5 @@ var updateBot = (bot, value) => {
     }
 };
 
-var updateOutput = (output, value) => {
-    if(value > outputs[output].high) {
-        outputs[output].low = outputs[output].high;
-        outputs[output].high = value;
-    } else if(value > outputs[output].low) {
-        outputs[output].low = value;
-    }
-};
+var updateOutput = (output, value) => !outputs[output] || value > outputs[output] ? outputs[output] = parseInt(value) : true;
 
