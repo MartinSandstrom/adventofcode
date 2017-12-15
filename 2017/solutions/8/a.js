2de0edee14b603634ecf09f7467670e01f12678e
Martin Sandström <martinsandstroms@gmail.com>
2017-12-15 23:44:45 +1100

    refactor

--- 2017/solutions/8/a.js
+++ 2017/solutions/8/a.js
@@ -18,15 +18,13 @@ let expressions = {
 	'dec': (a, b) => a += b
 };
 
-let doMagic = (data, allObjects) => {
-	return data.split('\n').filter(Boolean).reduce((val, line) => {
-		let instructions = line.split(' ');
-		allObjects[instructions[0]] ? true : allObjects[instructions[0]] = 0;
-		allObjects[instructions[4]] ? true : allObjects[instructions[4]] = 0;
-		if (expressions[instructions[5]](allObjects[instructions[4]], instructions[6])) {
-			instructions[1] === 'inc' ? allObjects[instructions[0]] += parseInt(instructions[2]) :
-				allObjects[instructions[0]] -= parseInt(instructions[2]);
-		}
-		return Math.max(val, allObjects[instructions[0]]);
-	}, 0);
-};
+let doMagic = (data, allObjects) => data.split('\n').filter(Boolean).reduce((val, line) => {
+	let instructions = line.split(' ');
+	allObjects[instructions[0]] ? true : allObjects[instructions[0]] = 0;
+	allObjects[instructions[4]] ? true : allObjects[instructions[4]] = 0;
+	if (expressions[instructions[5]](allObjects[instructions[4]], instructions[6])) {
+		instructions[1] === 'inc' ? allObjects[instructions[0]] += parseInt(instructions[2]) :
+			allObjects[instructions[0]] -= parseInt(instructions[2]);
+	}
+	return Math.max(val, allObjects[instructions[0]]);
+}, 0);
