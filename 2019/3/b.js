6714049d00dbd13f3e0ae5cbcc4738b80519b0e5
Martin Sandström <martinsandstroms@gmail.com>
2019-12-03 16:44:45 +1100

    Solving day 3

---
+++ 2019/3/b.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+
+fs.readFile("./data.txt", "utf8", function (error, data) {
+    if(error) {
+        console.log(error);
+    }
+    console.log('answer', doMagic(data));
+});
+
+const doMagic = input  => {
+  const array = input.split('\n').filter(Boolean);
+
+  const firstWire = array[0];
+  const secondWire = array[1];
+
+  return getShostestPath(firstWire, secondWire)
+}
+
+const getShostestPath = (firstWire, secondWire) => {
+  const visitedMap = {};
+  const intersectionMap = {};
+
+  placeWire(firstWire, visitedMap, intersectionMap, true);
+  placeWire(secondWire, visitedMap, intersectionMap, false);
+
+  const minIntersectionPoint = Object.values(intersectionMap).reduce((acc, curr) => Math.min(acc, curr), Infinity);
+  return minIntersectionPoint;
+}
+
+const placeWire = (array, visitedMap, intersectionMap, isFirstWire) => {
+  let x = 0;
+  let y = 0;
+  let totalSteps = 0;
+  array.split(',').forEach(i => {
+    const direction = i.slice(0, 1);
+    let numberOfSteps = i.slice(1, i.length);
+    while (numberOfSteps > 0) {
+      totalSteps++;
+      if (direction === 'U') y++;
+      if (direction === 'D') y--;
+      if (direction === 'L') x--;
+      if (direction === 'R') x++;
+
+      const key = `${x},${y}`;
+      if(isFirstWire) {
+        visitedMap[key] = totalSteps;
+      } else if (visitedMap[key]) {
+        intersectionMap[key] = visitedMap[key] + totalSteps;
+      }
+      numberOfSteps--;
+    }
+  });
+}
+
+
+console.log('TEST ONE = 610 => ', getShostestPath('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'));
+console.log('TEST TWO = 410 => ', getShostestPath('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'))
