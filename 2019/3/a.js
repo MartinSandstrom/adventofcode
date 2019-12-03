9c8a749d78ae41616fdefa975217d5b4edd194e9
Martin Sandström <martinsandstroms@gmail.com>
2019-12-03 17:06:42 +1100

    Refactoring

--- 2019/3/a.js
+++ 2019/3/a.js
@@ -13,7 +13,7 @@ const doMagic = input  => {
   const firstWire = array[0];
   const secondWire = array[1];
 
-  return getShostestPath(firstWire, secondWire)
+  return getShostestPath(firstWire, secondWire);
 }
 
 const getShostestPath = (firstWire, secondWire) => {
@@ -52,4 +52,4 @@ const placeWire = (array, visitedMap, isFirstWire) => {
 }
 
 console.log('TEST ONE = 159 => ', getShostestPath('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'));
-console.log('TEST TWO = 135 => ', getShostestPath('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'))
+console.log('TEST TWO = 135 => ', getShostestPath('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'));
