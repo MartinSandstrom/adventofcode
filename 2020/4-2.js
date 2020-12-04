19525ce7101588f95a340737c704796fbc125fde
Martin Sandström <martinsandstroms@gmail.com>
2020-12-04 15:25:37 +0100

    Small refactor

--- 2020/4-2.js
+++ 2020/4-2.js
@@ -1,10 +1,10 @@
 var isValidObject = {
-  byr: (value) => +value >= 1920 && +value <= 2002,
-  iyr: (value) => +value >= 2010 && +value <= 2020,
-  eyr: (value) => +value >= 2020 && +value <= 2030,
+  byr: (value) => value >= 1920 && value <= 2002,
+  iyr: (value) => value >= 2010 && value <= 2020,
+  eyr: (value) => value >= 2020 && value <= 2030,
   hgt: (value) =>
-    (value.includes("cm") && +value.split("cm")[0] >= 150 && +value.split("cm")[0] <= 193) ||
-    (value.includes("in") && +value.split("in")[0] >= 59 && +value.split("in")[0] <= 76),
+    (value.includes("cm") && value.split("cm")[0] >= 150 && value.split("cm")[0] <= 193) ||
+    (value.includes("in") && value.split("in")[0] >= 59 && value.split("in")[0] <= 76),
   hcl: (value) => RegExp("^#[a-f0-9]{6}").test(value),
   ecl: (value) => new Set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]).has(value),
   pid: (value) => RegExp("^[0-9]{9}$").test(value),
