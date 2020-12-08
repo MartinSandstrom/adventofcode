7bbac9dc8d8cf30dc8acfe06310cd06a57b89d32
Martin Sandström <martinsandstroms@gmail.com>
2020-12-08 12:34:13 +0100

    Refactor

--- 2020/8.js
+++ 2020/8.js
@@ -1,14 +1,15 @@
 var findRecursiveCount = (rows, index, allSeen, count) => {
   if (allSeen.has(index)) {
-    console.log("AllSeen", count);
+    console.log("Seen twice", count);
     return;
   }
   var [instruction, value] = rows[index].split(" ");
   allSeen.add(index);
-  if (instruction === "nop") findRecursiveCount(rows, ++index, allSeen, count);
-  else if (instruction === "acc") findRecursiveCount(rows, ++index, allSeen, count + eval(value));
-  else if (instruction === "jmp") findRecursiveCount(rows, index + eval(value), allSeen, count);
+  findRecursiveCount(
+    rows,
+    instruction === "jmp" ? index + eval(value) : ++index,
+    allSeen,
+    instruction === "acc" ? count + eval(value) : count
+  );
 };
-
-var rows = document.querySelector("pre").textContent.split("\n").filter(Boolean);
-findRecursiveCount(rows, 0, new Set(), 0);
+findRecursiveCount(document.querySelector("pre").textContent.split("\n").filter(Boolean), 0, new Set(), 0);
