1e9f2c6e3f9263df571a2c7757bf5ff96d9f0354
Martin Sandström <martinsandstroms@gmail.com>
2020-12-12 14:49:30 +0100

    Day 12

--- 2020/12/12.js
+++ 2020/12/12.js
@@ -8,34 +8,33 @@ fs.readFile("./pussel.txt", "utf8", function (error, data) {
   solvePartOne(all);
 });
 
-const replaceForwardWithDirection = (array, facing) => {
-  return array.reduce((newArray, instruction) => {
+const replaceForwardWithDirection = (array, facing) =>
+  array.reduce((fixedArray, instruction) => {
     var char = instruction[0];
     var number = +instruction.split(char)[1];
     var numberOfTurns = number / 90;
     if (char === "F") {
-      newArray.push(instruction.replace("F", facing));
-    } else {
-      if (char === "R") {
-        while (numberOfTurns--) {
-          if (facing === "N") facing = "E";
-          else if (facing === "E") facing = "S";
-          else if (facing === "S") facing = "W";
-          else if (facing === "W") facing = "N";
-        }
-      } else if (char === "L") {
-        while (numberOfTurns--) {
-          if (facing === "N") facing = "W";
-          else if (facing === "W") facing = "S";
-          else if (facing === "S") facing = "E";
-          else if (facing === "E") facing = "N";
-        }
+      fixedArray.push(instruction.replace("F", facing));
+    } else if (char === "R") {
+      while (numberOfTurns--) {
+        if (facing === "N") facing = "E";
+        else if (facing === "E") facing = "S";
+        else if (facing === "S") facing = "W";
+        else if (facing === "W") facing = "N";
       }
-      newArray.push(instruction);
+    } else if (char === "L") {
+      while (numberOfTurns--) {
+        if (facing === "N") facing = "W";
+        else if (facing === "W") facing = "S";
+        else if (facing === "S") facing = "E";
+        else if (facing === "E") facing = "N";
+      }
+    } else {
+      fixedArray.push(instruction);
     }
-    return newArray;
+
+    return fixedArray;
   }, []);
-};
 
 const solvePartOne = (array) => {
   var fixedArray = replaceForwardWithDirection(array, "E");
