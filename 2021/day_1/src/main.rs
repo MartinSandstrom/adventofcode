373b18906a0f8c93d74a320c5fd6252d3e763e32
Martin Sandström <martinsandstroms@gmail.com>
2021-12-01 13:40:11 +0100

    Solving day 1

--- 2021/day_1/src/main.rs
+++ 2021/day_1/src/main.rs
@@ -1,29 +1,39 @@
 use std::fs;
 
-fn part_one(data: Vec<i32>) {
+fn count(data: Vec<u32>) -> u32 {
     let mut count = 0;
     for tuple in data.windows(2) {
         if tuple[0] < tuple[1] {
             count += 1;
         }
     }
-
-    println!("{}", count);
+    return count;
 }
 
-fn part_two() {
-    let mut count = 0;
-    println!("{}", count);
+fn adapt_to_three_measurement(data: Vec<u32>) -> Vec<u32> {
+    let mut adapted_vec = Vec::<u32>::new();
+    for triple in data.windows(3) {
+        adapted_vec.push(triple[0] + triple[1] + triple[2]);
+    }
+    return adapted_vec;
+}
+
+fn part_one(data: Vec<u32>) {
+    println!("Part one: {}", count(data));
+}
+
+fn part_two(data: Vec<u32>) {
+    let adapted_data = adapt_to_three_measurement(data);
+    println!("Part two: {}", count(adapted_data));
 }
 
 fn main() {
     let contents = fs::read_to_string("./puzzle.txt").expect("Unable to open");
-    let data: Vec<_> = contents
-        .trim()
+    let data: Vec<u32> = contents
         .split('\n')
-        .map(|line| line.parse::<i32>().unwrap())
+        .map(|line| line.parse::<u32>().unwrap())
         .collect();
 
-    part_one(data);
-    part_two();
+    part_one(data.clone());
+    part_two(data.clone());
 }
