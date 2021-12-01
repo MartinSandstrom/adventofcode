d25daf00d1dd4fcbf0e66e28dd6269b41e39ca26
Martin Sandström <martinsandstroms@gmail.com>
2021-12-01 14:21:48 +0100

    Solving day 1 post reddit

--- 2021/day_1/src/main.rs
+++ 2021/day_1/src/main.rs
@@ -1,30 +1,18 @@
 use std::fs;
 
-fn count(data: Vec<u32>) -> u32 {
-    let mut count = 0;
-    for tuple in data.windows(2) {
-        if tuple[0] < tuple[1] {
-            count += 1;
-        }
-    }
-    return count;
-}
-
-fn adapt_to_three_measurement(data: Vec<u32>) -> Vec<u32> {
-    let mut adapted_vec = Vec::<u32>::new();
-    for triple in data.windows(3) {
-        adapted_vec.push(triple[0] + triple[1] + triple[2]);
-    }
-    return adapted_vec;
-}
 
 fn part_one(data: Vec<u32>) {
-    println!("Part one: {}", count(data));
+    let count = data.windows(2)
+        .filter(|w| w[1] > w[0])
+        .count();
+    println!("Part one: {}", count);
 }
 
 fn part_two(data: Vec<u32>) {
-    let adapted_data = adapt_to_three_measurement(data);
-    println!("Part two: {}", count(adapted_data));
+    let count = data.windows(4)
+        .filter(|w| w[3] > w[0])
+        .count();
+    println!("Part two: {}", count);
 }
 
 fn main() {
