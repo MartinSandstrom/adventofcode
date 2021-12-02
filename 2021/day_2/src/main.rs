5820ff4c6aea5f104d258baddf721fb374aa2b07
Martin Sandström <martinsandstroms@gmail.com>
2021-12-02 09:20:44 +0100

    Day 2

--- 2021/day_2/src/main.rs
+++ 2021/day_2/src/main.rs
@@ -1,11 +1,62 @@
 use std::fs;
 
 
-fn part_one(data: String) {
-    print!("Part one: {:?}", data);
+fn part_one(data: Vec<&str>) {
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+
+    for s in data.iter() {
+        let chunks: Vec<_> = s.split_whitespace().collect();        
+        let dir = chunks[0];
+        let value = chunks[1].parse::<i32>().unwrap();
+
+        if dir == "forward" {
+            horizontal += value;
+        }
+        else if dir == "down" {
+            depth -= value;
+        }
+        else if dir == "up" {
+            depth += value;
+        }
+    }
+    
+    println!("Part one {:?}", horizontal * depth);
+}
+
+fn part_two(data: Vec<&str>) {
+
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;    
+    let mut aim: i32 = 0;    
+
+    for s in data.iter() {
+        let chunks: Vec<_> = s.split_whitespace().collect();        
+        let dir = chunks[0];
+        let value = chunks[1].parse::<i32>().unwrap();
+
+        if dir == "forward" {
+            horizontal += value;
+            depth += value * aim;
+        }
+        else if dir == "down" {
+            aim -= value;
+        }
+        else if dir == "up" {
+            aim += value;
+        }
+    }
+    
+    println!("Part one {:?}", horizontal * depth);
 }
 
 fn main() {
     let contents = fs::read_to_string("./puzzle.txt").expect("Unable to open");
-    part_one(contents);
+    let data: Vec<_> = contents 
+                .split('\n')
+                .collect();
+
+    part_one(data.clone());
+    part_two(data.clone());
+
 }
