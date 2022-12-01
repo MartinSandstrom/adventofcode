581a3a49efb94c4cf560af9f25de5bd316122bb2
Martin Sandström <martinsandstroms@gmail.com>
2022-12-01 20:22:08 +0100

    2022 Day 1

--- year_2022/src/day_1.rs
+++ year_2022/src/day_1.rs
@@ -1,20 +1,19 @@
 use std::fs;
 
 pub unsafe fn solve_puzzle_of_the_day() {
-    let content = fs::read_to_string("./src/day_1_puzzle.txt").expect("Unable to open");
-    let mut all_elf_meals: Vec<i32> = 
-        content
+    let content: String = fs::read_to_string("./src/day_1_puzzle.txt").expect("Unable to open");
+    let mut all_elf_total_food: Vec<i32> = content
         .split("\n\n")
-        .map(|food| {
+        .map(|food: &str| {
             food.split('\n')
-                .map(|meal| meal.parse::<i32>().unwrap())
+                .map(|meal: &str| meal.parse::<i32>().unwrap())
                 .sum()
         })
         .collect();
-    all_elf_meals.sort();
-    let first = all_elf_meals.pop().unwrap();
+    all_elf_total_food.sort();
+    let first: i32 = all_elf_total_food.pop().unwrap();
     println!("Day 1 part one: {:?}", first);
-    let second = all_elf_meals.pop().unwrap();
-    let third = all_elf_meals.pop().unwrap();
+    let second: i32 = all_elf_total_food.pop().unwrap();
+    let third: i32 = all_elf_total_food.pop().unwrap();
     println!("Day 1 part two: {:?}", first + second + third);
 }
